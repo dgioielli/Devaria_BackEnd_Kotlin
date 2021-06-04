7e49c4b76b9645eb5df2f464099d5260459454d8
Douglas Gioielli <douglasgioielli@gmail.com>
2021-06-03 23:21:26 -0300

    *** Conclusão da segunda Aula

---
+++ Gerenciador-de-Projetos/src/main/kotlin/br/com/devaria/Projetos/Gerenciador/de/Projetos/GerenciadorDeProjetosApplication.kt
@@ -0,0 +1,11 @@
+package br.com.devaria.Projetos.Gerenciador.de.Projetos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GerenciadorDeProjetosApplication
+
+fun main(args: Array<String>) {
+	runApplication<GerenciadorDeProjetosApplication>(*args)
+}
---
+++ Gerenciador-de-Projetos/src/test/kotlin/br/com/devaria/Projetos/Gerenciador/de/Projetos/GerenciadorDeProjetosApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.devaria.Projetos.Gerenciador.de.Projetos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GerenciadorDeProjetosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
